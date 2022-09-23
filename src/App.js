a90e6672947cc1b1f4b319fa6d36ae317f8f5d01
Khaled <khalegjr@gmail.com>
2022-09-23 18:20:11 -0300

    feat(head): Cria estrutura base do head

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/img/Logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="my-3">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand>
+              <img src={logo} className="mx-6" alt="logo" />
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
       </header>
     </div>
   );
