c2ecf3fa00ddd530977ee9b806a9436974c52ba2
Khaled <khalegjr@gmail.com>
2022-09-23 18:57:37 -0300

    feat: Cria o background da sessão um

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./assets/img/Logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Navbar, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header class="my-3">
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
           <Container>
             <Navbar.Brand>
               <img src={logo} className="mx-6" alt="logo" />
@@ -15,6 +15,16 @@ function App() {
           </Container>
         </Navbar>
       </header>
+
+      <main class="sessa1">
+        <Container fluid>
+          <Row>
+            <Col className="mx-6" bg="dark">
+              1 of 1
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </div>
   );
 }
