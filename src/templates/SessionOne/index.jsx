79f7ff2f41224c30d9b97e942729dad4936e7175
Khaled <khalegjr@gmail.com>
2022-09-25 20:34:17 -0300

    refactor(component): Move o carousel para um componente próprio

--- src/templates/SessionOne/index.jsx
+++ src/templates/SessionOne/index.jsx
@@ -2,65 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import CardReceita from "../../components/CardReceita";
 import { Col, Container, Row } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
+import CarouselSession from "../../components/Carousel";
 
 const SessionOne = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-
   return (
     <>
-      <section class="sessao2 justify-content-center align-items-center">
+      <section class="sessao2">
         <Container className="text-black">
           <Row>
             <Col
               className="mx-6 py-5 justify-content-center align-items-center"
               bg="dark"
             >
-              <Carousel
-                responsive={responsive}
-                sliderClass=""
-                slidesToSlide={1}
-                swipeable
-                infinite
-              >
-                <Container className="align-items-center">
-                  <CardReceita />
-                </Container>
-
-                <Container className="align-items-center">
-                  <CardReceita />
-                </Container>
-
-                <Container className="align-items-center">
-                  <CardReceita />
-                </Container>
-
-                <Container className="align-items-center">
-                  <CardReceita />
-                </Container>
-
-                <Container className="align-items-center">
-                  <CardReceita />
-                </Container>
-              </Carousel>
+              <CarouselSession />
             </Col>
           </Row>
         </Container>
