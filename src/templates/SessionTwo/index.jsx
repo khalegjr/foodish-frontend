bd0eff56cf51d42d2d9dca68a6dfcf2d4152950c
Khaled <khalegjr@gmail.com>
2022-09-26 15:33:33 -0300

    refactor(session two): Fix a responsividade da sessão e posicionamentos

--- src/templates/SessionTwo/index.jsx
+++ src/templates/SessionTwo/index.jsx
@@ -1,50 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 import imageRef from "../../assets/img/5c2e894fed6dddaf5b0998129bafd941.png";
 
 const SessionTwo = () => {
   return (
     <>
-      <section class="sessao3">
-        <Container>
-          <Row>
-            <Col sm={6}>
-              <Container>
-                <h2
-                  style={{
-                    /* UI Properties */
-                    textAlign: "left",
-                    font: "normal normal bold 48px/96px Poppins",
-                    letterSpacing: "0px",
-                    color: "#FFFFFF",
-                    opacity: "1",
-                  }}
-                >
-                  Lorem ipsum sit
-                </h2>
+      <Container as="section" className="w-100 bg-secondary" fluid>
+        <Container className="py-4 py-lg-5">
+          <Row className="pt-4">
+            <Col sm={12} lg={6}>
+              <Container className="text-white mt-2">
+                <h2 className="mt-1 mb-5">Lorem ipsum sit</h2>
 
-                <p
-                  style={{
-                    textAlign: "left",
-                    font: "normal normal normal 22px/33px Poppins",
-                    letterSpacing: "0px",
-                    color: "#FFFFFF",
-                    opacity: "1",
-                  }}
-                >
+                <p className="m-0">
                   Amet, consectetur adipiscing elit, sed do eiusmod tempor
                   incididunt ut labore et dolore magna aliqua.
                 </p>
-                <p
-                  style={{
-                    textAlign: "left",
-                    font: "normal normal normal 22px/33px Poppins",
-                    letterSpacing: "0px",
-                    color: "#FFFFFF",
-                    opacity: "1",
-                  }}
-                >
+                <p className="my-5">
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute
                   irure dolor in reprehenderit in voluptate velit esse cillum
@@ -52,25 +25,18 @@ const SessionTwo = () => {
                 </p>
               </Container>
             </Col>
-            <Col sm={6}>
-              <Card className="bg-dark text-white p-0 m-0">
-                <Card.Img
-                  src={imageRef}
-                  alt="Card image"
-                  style={{
-                    width: "650px",
-                    height: "384px",
-                    /* UI Properties */
-                    opacity: "1",
-                    margin: 0,
-                    padding: 0,
-                  }}
-                />
-              </Card>
+            <Col lg={6}>
+              <Image
+                sr
+                className="pb-4 m-0"
+                fluid
+                src={imageRef}
+                alt="Card image"
+              />
             </Col>
           </Row>
         </Container>
-      </section>
+      </Container>
     </>
   );
 };
