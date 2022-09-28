95c1449c3ff503fadd54c3b7a3bbc624cb9d7634
Khaled <khalegjr@gmail.com>
2022-09-27 22:50:57 -0300

    refactor(principalcard): ajustes de estilização

--- src/templates/PrincipalCard/index.jsx
+++ src/templates/PrincipalCard/index.jsx
@@ -6,32 +6,14 @@ import "./style.scss";
 const SessionMain = () => {
   return (
     <>
-      <Container className="mt-lg-1 principal-card">
-        <Row className="m-2 mt-lg-5 pt-lg-5">
-          <h1
-            className="text-white"
-            // style={{
-            //   maxWidth: "689px",
-            // }}
-          >
-            Teste o estilo de vida
-          </h1>
-        </Row>
-
-        <Row className="text-white py-2" sd={12} lg={7}>
-          <h3>
-            H3: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          </h3>
-        </Row>
-
-        {/* TODO: Fix o background */}
-        <Row className=" py-2 py-lg-5 ">
-          <Container className="px-0 pb-lg-1 pt-lg-3 mt-lg-1 m-sm-0 p-sm-2">
-            <Button size="lg" variant="primary" className="mb-lg-5">
-              Vamos lá!
-            </Button>
-          </Container>
-        </Row>
+      <Container className="principal-card" fluid>
+        <h1 className="text-white">Teste o estilo de vida </h1>
+        <h3 className="text-white ">
+          H3: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        </h3>
+        <Button size="lg" variant="primary" className="lets-go-button">
+          Vamos lá!
+        </Button>
       </Container>
     </>
   );
